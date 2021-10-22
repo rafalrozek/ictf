43bbf804f43b86a5955cd00a97a0bea8696f52bf
rafalrozek <rafal98.poczta@gmail.com>
2021-10-22 21:49:46 +0200

    Spring init

---
+++ src/test/java/pl/rafalrozek/ictf/IctfApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.rafalrozek.ictf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IctfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
